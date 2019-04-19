29ddbcc7257a1f80c780e70e973193163ecb1b7f
Ivan Solyankin <vanzef@gmail.com>
2019-04-19 18:03:31 +0300

    pythonPackages.node2vec: init at 0.3.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -3368,6 +3368,8 @@ in {
 
   nodeenv = callPackage ../development/python-modules/nodeenv { };
 
+  node2vec = callPackage ../development/python-modules/node2vec { };
+
   nose = callPackage ../development/python-modules/nose { };
 
   nose-cov = callPackage ../development/python-modules/nose-cov { };
